4a7fb69ce3284470f81ffaadc8d688ee2f0feb29
gsbarbo <gagebarbour96@gmail.com>
2022-12-25 09:16:17 -0600

    Moved routes to make more organized

---
+++ routes/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\LinkSteamController;
+use App\Http\Controllers\Auth\LogoutController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+
+Route::get('login/discord', function () {
+    return Socialite::driver('discord')->redirect();
+})->name('auth.discord')->middleware('guest');
+
+Route::get('login/discord/handle', [AuthController::class, 'login'])->middleware('guest');
+
+Route::get('link/steam', function () {
+    return Socialite::driver('steam')->redirect();
+})->name('auth.steam')->middleware('guest');
+
+Route::get('/link/steam/handle', [LinkSteamController::class, 'link'])->middleware('guest');
+
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+Route::resource('account', AccountController::class, ['parameters' => ['account' => 'user']]);
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Auth\LinkSteamController;
-use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,18 +16,10 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::view('/', 'pages.home')->name('home');
 
-Route::get('login/discord', function () {
-    return Socialite::driver('discord')->redirect();
-})->name('auth.discord')->middleware('guest');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('apply', ApplyController::class, ['parameters' => [
+        'apply' => 'application'
+    ]]);
+});
 
-Route::get('login/discord/handle', [AuthController::class, 'login'])->middleware('guest');
-
-Route::get('link/steam', function () {
-    return Socialite::driver('steam')->redirect();
-})->name('auth.steam')->middleware('guest');
-
-Route::get('/link/steam/handle', [LinkSteamController::class, 'link'])->middleware('guest');
-
-Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
-
-Route::resource('account', AccountController::class, ['parameters' => ['account' => 'user']]);
+require __DIR__ . '/auth.php';
