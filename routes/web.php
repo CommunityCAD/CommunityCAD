e4e43f4a25398151947bb941fc0af67237bcaa26
gsbarbo <gagebarbour96@gmail.com>
2022-12-28 15:47:09 -0600

    started applicaiton

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.home')->name('home');
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('apply', ApplyController::class, ['parameters' => [
-        'apply' => 'application'
-    ]]);
+    Route::resource('application', ApplicationController::class);
 });
 
 require __DIR__ . '/auth.php';
