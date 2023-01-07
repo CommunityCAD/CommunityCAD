482616ce88bff76970808f0410d1d75db3650186
gsbarbo <gagebarbour96@gmail.com>
2023-01-07 17:40:42 -0600

    Fixed application status name for account profile

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Application extends Model
 {
@@ -18,13 +19,20 @@ class Application extends Model
         'updated_at' => 'datetime',
     ];
 
-    public function apply(Builder $builder, Model $model)
-    {
-        $builder->orderBy('id', 'asc');
-    }
-
     public function user()
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function getDepartmentNameAttribute()
+    {
+        $department_name = Department::where('id', '=', $this->department_id)->first();
+        return $department_name->name;
+    }
+
+    public function getStatusNameAttribute()
+    {
+        $status_name = DB::table('application_statuses')->where('id', '=', $this->status)->first();
+        return $status_name->name;
     }
 }
--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -122,8 +122,8 @@
                                 <li>
                                     <a href="{{ route('application.show', $application->id) }}"
                                         class="text-teal-300 hover:underline hover:text-teal-400">Application
-                                        #{{ $application->id }}</a>
-                                    <div class="text-xs">Status: Pending Review</div>
+                                        #{{ $application->id }} for {{ $application->department_name }}</a>
+                                    <div class="text-xs">Status: {{ $application->status_name }}</div>
                                     <div class="text-xs">Created At: {{ $application->created_at->format('m/d/Y') }}</div>
                                 </li>
                             @endforeach
