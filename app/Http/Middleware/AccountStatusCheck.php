3577fba9878f2051f99d75842406ad070388f73a
gsbarbo <gagebarbour96@gmail.com>
2023-01-07 18:03:37 -0600

    Added securtiy mesures to account pages

--- app/Http/Middleware/AccountStatusCheck.php
+++ app/Http/Middleware/AccountStatusCheck.php
@@ -8,21 +8,21 @@ use Illuminate\Http\Request;
 
 class AccountStatusCheck
 {
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle(Request $request, Closure $next)
-	{
-		if ($request->user()) {
-			if ($request->user()->account_status >= 4) {
-				return abort(403);
-			}
-		}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user()) {
+            if ($request->user()->account_status >= 4) {
+                return abort(403, $request->user()->account_status_name . '. Contact an Admin if you fell this is wrong.');
+            }
+        }
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 }
