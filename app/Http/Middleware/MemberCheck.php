5aa08fbbdc4423a7a843edccda49c9873641cfeb
gsbarbo <gagebarbour96@gmail.com>
2023-03-19 23:57:01 -0500

    Fixed Member Check for Portal Pages

--- app/Http/Middleware/MemberCheck.php
+++ app/Http/Middleware/MemberCheck.php
@@ -7,20 +7,20 @@ use Illuminate\Http\Request;
 
 class MemberCheck
 {
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle(Request $request, Closure $next)
-	{
-		if ($request->user()) {
-			if ($request->user()->account_status != 2) {
-				return abort(403);
-			}
-		}
-		return $next($request);
-	}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user()) {
+            if ($request->user()->account_status != 3) {
+                return abort(403);
+            }
+        }
+        return $next($request);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,50 +35,52 @@ Route::view('/', 'pages.home')->name('home');
 Route::middleware(['auth'])->group(function () {
     Route::resource('application', ApplicationController::class);
 });
+Route::middleware(['auth', 'member.check'])->group(function () {
+    Route::name('portal.')->prefix('portal')->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::middleware(['auth'])->name('portal.')->prefix('portal')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('department/{department}', [DepartmentController::class, 'show'])->name('department.show');
+    });
 
-    Route::get('department/{department}', [DepartmentController::class, 'show'])->name('department.show');
+    Route::name('cad.')->prefix('cad')->group(function () {
+        Route::get('landing', [PageController::class, 'index'])->name('landing');
+    });
+
+    Route::name('civilian.')->prefix('civilian')->group(function () {
+        Route::resource('civilians', CivilianController::class);
+    });
+
+    Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function () {
+        Route::get('application/status/{status?}', [AdminApplicationController::class, 'index'])->name('application.index');
+
+        Route::get('/application/approve_application/{application}', ApproveApplicationController::class)->name('application.approve_application');
+        Route::get('/application/approve_interview/{application}', ApproveInterviewController::class)->name('application.approve_interview');
+
+        Route::get('/application/deny_application/{application}', [DenyApplicationController::class, 'edit'])->name('application.deny_application.edit');
+        Route::put('/application/deny_application/{application}', [DenyApplicationController::class, 'store'])->name('application.deny_application.store');
+
+        Route::get('/application/flag_application/{application}', [FlagApplicationController::class, 'edit'])->name('application.flag_application.edit');
+        Route::put('/application/flag_application/{application}', [FlagApplicationController::class, 'store'])->name('application.flag_application.store');
+
+        Route::get('/application/deny_interview/{application}', [DenyInterviewController::class, 'edit'])->name('application.deny_interview.edit');
+        Route::put('/application/deny_interview/{application}', [DenyInterviewController::class, 'store'])->name('application.deny_interview.store');
+
+        Route::get('application/{application}', [AdminApplicationController::class, 'show'])->name('application.show');
+
+
+        Route::resource('/roles', RoleController::class);
+        Route::resource('/permissions', PermissionController::class);
+
+        Route::get('/users/advanced', [UserController::class, 'all_users'])->name('users.advanced.index');
+        Route::get('/user/{user}/roles/edit', [UserRoleController::class, 'edit'])->name('users.roles.edit');
+        Route::put('/user/{user}/roles', [UserRoleController::class, 'update'])->name('users.roles.update');
+        Route::get('/user/{user}/status/edit', [UserStatusController::class, 'edit'])->name('users.status.edit');
+        Route::put('/user/{user}/status', [UserStatusController::class, 'update'])->name('users.status.update');
+
+        Route::resource('/users', UserController::class);
+    });
 });
 
-Route::middleware(['auth'])->name('cad.')->prefix('cad')->group(function () {
-    Route::get('landing', [PageController::class, 'index'])->name('landing');
-});
-
-Route::middleware(['auth'])->name('civilian.')->prefix('civilian')->group(function () {
-    Route::resource('civilians', CivilianController::class);
-});
-
-Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function () {
-    Route::get('application/status/{status?}', [AdminApplicationController::class, 'index'])->name('application.index');
-
-    Route::get('/application/approve_application/{application}', ApproveApplicationController::class)->name('application.approve_application');
-    Route::get('/application/approve_interview/{application}', ApproveInterviewController::class)->name('application.approve_interview');
-
-    Route::get('/application/deny_application/{application}', [DenyApplicationController::class, 'edit'])->name('application.deny_application.edit');
-    Route::put('/application/deny_application/{application}', [DenyApplicationController::class, 'store'])->name('application.deny_application.store');
-
-    Route::get('/application/flag_application/{application}', [FlagApplicationController::class, 'edit'])->name('application.flag_application.edit');
-    Route::put('/application/flag_application/{application}', [FlagApplicationController::class, 'store'])->name('application.flag_application.store');
-
-    Route::get('/application/deny_interview/{application}', [DenyInterviewController::class, 'edit'])->name('application.deny_interview.edit');
-    Route::put('/application/deny_interview/{application}', [DenyInterviewController::class, 'store'])->name('application.deny_interview.store');
-
-    Route::get('application/{application}', [AdminApplicationController::class, 'show'])->name('application.show');
-
-
-    Route::resource('/roles', RoleController::class);
-    Route::resource('/permissions', PermissionController::class);
-
-    Route::get('/users/advanced', [UserController::class, 'all_users'])->name('users.advanced.index');
-    Route::get('/user/{user}/roles/edit', [UserRoleController::class, 'edit'])->name('users.roles.edit');
-    Route::put('/user/{user}/roles', [UserRoleController::class, 'update'])->name('users.roles.update');
-    Route::get('/user/{user}/status/edit', [UserStatusController::class, 'edit'])->name('users.status.edit');
-    Route::put('/user/{user}/status', [UserStatusController::class, 'update'])->name('users.status.update');
-
-    Route::resource('/users', UserController::class);
-});
 
 
 
