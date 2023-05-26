abd7df89a87b009ab8aa47735b49c3b0475e6852
gsbarbo <gagebarbour96@gmail.com>
2023-05-25 21:52:50 -0500

    update User Seed

--- database/db_defaults/users.sql
+++ database/db_defaults/users.sql
@@ -13,8 +13,8 @@
 
 -- Dumping data for table fivemcad.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `discord_name`, `discriminator`, `avatar`, `steam_hex`, `steam_id`, `steam_name`, `account_status`, `reapply`, `reapply_date`, `denied_reason`, `email`, `real_name`, `birthday`, `display_name`, `ts_name`, `officer_name`, `last_login`, `is_protected_user`, `is_super_user`, `history`, `civilian_level_id`, `remember_token`, `deleted_at`, `created_at`, `updated_at`, `member_join_date`) VALUES
-	(188790560658685954, 'Ron Swanson', '1776', 'https://cdn.discordapp.com/avatars/188790560658685954/a_92e9cc29feaeb37cb66418a8d320faf0.gif', '11000010CDE9587', 76561198176179591, 'Loaf Dog', 3, NULL, NULL, NULL, 'asd@asdf.com', 'Ron Swanson', '1776-07-04', NULL, NULL, NULL, '2023-05-16 14:08:02', 1, 1, NULL, 1, 'YDynCFaO6Xql6uJj7RnH0G0qow1JaJN30ZfBxaL4yJd0evGvT2kkBRgzfarI', NULL, '2023-03-19 23:28:59', '2023-05-16 15:12:13', '2023-04-02 15:08:49');
+INSERT INTO `users` (`id`, `discord_name`, `discriminator`, `avatar`, `steam_hex`, `steam_id`, `steam_name`, `account_status`, `reapply`, `reapply_date`, `denied_reason`, `email`, `real_name`, `birthday`, `display_name`, `ts_name`, `officer_name`, `last_login`, `is_protected_user`, `is_super_user`, `civilian_level_id`, `remember_token`, `deleted_at`, `created_at`, `updated_at`, `member_join_date`) VALUES
+	(188790560658685954, 'Ron Swanson', '1776', 'https://cdn.discordapp.com/avatars/188790560658685954/a_92e9cc29feaeb37cb66418a8d320faf0.gif', '11000010CDE9587', 76561198176179591, 'Loaf Dog', 3, NULL, NULL, NULL, 'asd@asdf.com', 'Ron Swanson', '1776-07-04', NULL, NULL, NULL, '2023-05-16 14:08:02', 1, 1, 1, 'YDynCFaO6Xql6uJj7RnH0G0qow1JaJN30ZfBxaL4yJd0evGvT2kkBRgzfarI', NULL, '2023-03-19 23:28:59', '2023-05-16 15:12:13', '2023-04-02 15:08:49');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
