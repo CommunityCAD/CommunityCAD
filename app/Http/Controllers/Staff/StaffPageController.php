b45a2377e2ef18aa538d6a8414c0358392a2bc25
gsbarbo <gagebarbour96@gmail.com>
2023-06-13 19:12:16 -0500

    Undo changes to Staff page from testing

--- app/Http/Controllers/Staff/StaffPageController.php
+++ app/Http/Controllers/Staff/StaffPageController.php
@@ -8,10 +8,6 @@ class StaffPageController extends Controller
 {
     public function index()
     {
-        $user = 1117962582905585684;
-        // dd($new_arr);
-
-        dd(in_array($user, config('cad.owner_ids')));
         return view('staff.index');
     }
 }
