932fc51f37c0377e7e77b19a6e483c527029867f
gsbarbo <gagebarbour96@gmail.com>
2023-07-23 16:29:01 -0500

    Add new Permission for penal codes

---
+++ database/migrations/2023_07_23_162812_add_new_permission_penalcode_to_permissions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('permissions')->insert(['title' => 'penal_code_manage', 'category' => 'admin', 'description' => 'Grants access to manage the penal code.']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            //
+        });
+    }
+};
