5cd7e5743d0979a2fb2cce0ca3de34ae5ab63836
gsbarbo <gagebarbour96@gmail.com>
2023-11-25 11:53:43 -0600

    added penal code default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             'database/db_defaults/civilian_levels.sql',
             'database/db_defaults/departments.sql',
             'database/db_defaults/licenses_defaults.sql',
+            'database/db_defaults/penal_code.sql',
             'database/db_defaults/permissions.sql',
             'database/db_defaults/statuses.sql',
             'database/db_defaults/users.sql',
