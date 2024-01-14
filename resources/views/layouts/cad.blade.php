dbd7bab5dce28e816b15053bd9967c3061dfb140
Gage B. <gagebarbour96@gmail.com>
2024-01-13 21:52:27 -0600

    added bottom bar back

--- resources/views/layouts/cad.blade.php
+++ resources/views/layouts/cad.blade.php
@@ -65,7 +65,7 @@
     @endif
     <div class="relative bg-red-700">
         <div class="fixed bottom-0 left-0 z-50 w-full">
-            <div class="bg-slate-500 z-50 w-full">
+            <div class="bg-slate-300 z-50 w-full flex justify-between items-center">
                 <p class="flex items-center space-x-4">
                     <span class=" ml-4">
                         @if (auth()->user()->active_unit->status == 'OFFDTY')
@@ -92,6 +92,15 @@
                     </span>
                     <span class="">Messages: 0</span>
                     <span class="">No Alerts</span>
+                </p>
+                <p class="flex">
+                    <svg class="w-6 h-6 text-green-700" fill="none" stroke-width="1.5" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+                            stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <span class="mx-3">Connected to live_database_prod</span>
                 </p>
             </div>
         </div>
