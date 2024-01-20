1e5f700fe7ec5bade9355f72b8b7abb7991cef04
Gage B. <gagebarbour96@gmail.com>
2024-01-19 19:54:56 -0600

    Fixed issue with createing calls

--- resources/views/cad/calls/create.blade.php
+++ resources/views/cad/calls/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex flex-col uppercase">
-        @include('inc.cad.header', ['active_unit' => auth()->user()->active_unit->civilian->name])
+        @include('inc.cad.header')
         <div class="p-4 mt-5 space-y-3 text-white rounded cursor-default">
 
             <livewire:cad.call-create-screen />
