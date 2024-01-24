a588eb14c1042ba02438602f8af5403045fd889b
Gage B. <gagebarbour96@gmail.com>
2024-01-23 19:56:40 -0600

    Updated counters on home page to not show my accounts

--- app/Http/Controllers/Portal/DashboardController.php
+++ app/Http/Controllers/Portal/DashboardController.php
@@ -13,10 +13,10 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        $total_members = User::where('account_status', 3)->count() - 2;
+        $total_members = User::where('account_status', 3)->where('id', '!=', 188790560658685954)->where('id', '!=', 1117962582905585684)->count();
         $announcements = Announcement::where('department_id', 0)->latest()->limit(5)->get();
 
-        $total_active_members = User::where('last_login', '>=', Carbon::now()->subDays(get_setting('days_until_inactive')))->count() - 2;
+        $total_active_members = User::where('last_login', '>=', Carbon::now()->subDays(get_setting('days_until_inactive')))->where('id', '!=', 188790560658685954)->where('id', '!=', 1117962582905585684)->count();
 
         return view('portal.dashboard', compact('total_members', 'total_active_members', 'announcements'));
     }
