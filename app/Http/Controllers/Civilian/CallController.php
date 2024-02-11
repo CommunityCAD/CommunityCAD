0ffad27b784e7bff9da9db2440939ccf17f802be
Gage B. <gagebarbour96@gmail.com>
2024-02-11 13:48:06 -0600

    fixed bug with civilians making calls

--- app/Http/Controllers/Civilian/CallController.php
+++ app/Http/Controllers/Civilian/CallController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Civilian;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Civilian\CallStoreRequest;
-use App\Models\Cad\Call;
 use App\Models\Cad\CallNatures;
+use App\Models\Call;
+use App\Models\CallCivilian;
 use App\Models\Civilian;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -22,12 +23,26 @@ class CallController extends Controller
     public function store(Civilian $civilian, CallStoreRequest $request): RedirectResponse
     {
         $input = $request->validated();
-        $input['priority'] = '1';
+        $civilian_id = false;
+
+        if ($input['civilian_id']) {
+            $civilian_id = $input['civilian_id'];
+            unset($input['civilian_id']);
+        }
+
+        $input['priority'] = '3';
         $input['source'] = '911 CALL';
         $input['status'] = 'RCVD';
-        $input['units'] = '{"data":[]}';
 
         $call = Call::create($input);
+
+        if ($civilian_id) {
+            CallCivilian::create([
+                'call_id' => $call->id,
+                'civilian_id' => $civilian_id,
+                'type' => 'RP',
+            ]);
+        }
 
         return redirect()->route('civilian.civilians.show', $civilian->id)->with('alerts', [['message' => '911 Call Created', 'level' => 'success']]);
     }
