e28744e7442c938772f813009f0290789ca49e32
Gage B. <gagebarbour96@gmail.com>
2024-04-28 18:02:25 -0500

    Fixed issue #41

--- app/Http/Controllers/Portal/DashboardController.php
+++ app/Http/Controllers/Portal/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
         $total_members = User::where('account_status', 3)->where('id', '!=', 188790560658685954)->where('id', '!=', 1117962582905585684)->count();
         $announcements = Announcement::where('department_id', 0)->latest()->limit(5)->get();
 
-        $total_active_members = User::where('last_login', '>=', Carbon::now()->subDays(get_setting('days_until_inactive')))->where('id', '!=', 188790560658685954)->where('id', '!=', 1117962582905585684)->count();
+        $total_active_members = User::where('account_status', 3)->where('last_login', '>=', Carbon::now()->subDays(get_setting('days_until_inactive')))->where('id', '!=', 188790560658685954)->where('id', '!=', 1117962582905585684)->count();
 
         return view('portal.dashboard', compact('total_members', 'total_active_members', 'announcements'));
     }
