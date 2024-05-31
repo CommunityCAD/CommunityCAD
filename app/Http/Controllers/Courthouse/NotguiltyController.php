861470206cf2426c810395319de173daf5a081c0
Gage B. <gagebarbour96@gmail.com>
2024-05-31 15:31:32 -0500

    Fixed so if found guilty license and vehicle stay suspended

--- app/Http/Controllers/Courthouse/NotguiltyController.php
+++ app/Http/Controllers/Courthouse/NotguiltyController.php
@@ -24,8 +24,10 @@ class NotguiltyController extends Controller
 
         $ticket->update($validated);
 
-        $ticket->vehicle()->update(['impound_ticket_id' => null, 'vehicle_status' => 1, 'registration_expire' => now()->addDays(30)]);
-        $ticket->license()->update(['suspend_ticket_id' => null, 'license_status' => 1, 'expires_on' => now()->addDays(30)]);
+        if ($validated['plea_type'] == 3) {
+            $ticket->vehicle()->update(['impound_ticket_id' => null, 'vehicle_status' => 1, 'registration_expire' => now()->addDays(30)]);
+            $ticket->license()->update(['suspend_ticket_id' => null, 'license_status' => 1, 'expires_on' => now()->addDays(30)]);
+        }
 
         return redirect()->route('courthouse.not_guilty.index')->with('alerts', [['message' => 'Ticket Updated.', 'level' => 'success']]);
     }
