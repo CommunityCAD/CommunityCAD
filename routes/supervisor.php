ac83c429325261123a7894b9694a2807f88db3c9
Gage B. <gagebarbour96@gmail.com>
2024-06-24 17:02:06 -0500

    Small Bug Fixes

--- routes/supervisor.php
+++ routes/supervisor.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Supervisor\BusinessController;
 use App\Http\Controllers\Supervisor\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('reports', ReportController::class)->only('index', 'show')->middleware('can:report_manage');
+Route::resource('reports', ReportController::class)->only('index', 'show')->middleware('can:reports_manage');
 
 Route::get('businesses/{status?}', [BusinessController::class, 'index'])->name('businesses.index')->middleware('can:business_manage');
 Route::get('businesses/view/{business}', [BusinessController::class, 'show'])->name('businesses.show')->middleware('can:business_manage');
